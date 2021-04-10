0e08cb8522732844c6d30acdf482bb8935b622b3
Robin-Lee03 <bnmgv283@gmail.com>
2021-04-10 18:08:37 +0800

    文字檔處理

---
+++ practice/文字檔字數.py
@@ -0,0 +1,16 @@
+def read_file():
+    lines = []
+    with open('text(71).txt','r')as f:
+        for line in f:
+            lines.append(line.strip())
+        return lines
+
+word_count = 1
+lines = read_file()
+for i in lines[0]:
+    if i == ' ':
+        word_count += 1
+print(lines)
+print('總共有', len(lines), '行')
+print('總共有', word_count, '個字')
+
